2733531c8affc001d7af64fea24aedb7facb9da7
Carlos Loyola <cloyola@users.noreply.github.com>
2025-07-16 00:47:15 -0300

    Update generate_deeplink.py

--- generate_deeplink.py
+++ generate_deeplink.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from logger import logger
 from generate_text import normalize
+import urllib.parse
 
 def generate_deeplink_simple(companyName, service_id, consumer = None):
   logger.info(f"Iniciando módulo: generate_deeplink.py")
@@ -18,11 +19,12 @@ def generate_deeplink_simple(companyName, service_id, consumer = None):
   company_id = company_selected.iloc[0]["company_id"]
   ruta_logo = company_selected.iloc[0]["ruta_logo_deeplink"]
   logger.info(f"Codigo de pago consumer {normalize(consumer)}")
+  company_encoded = urllib.parse.quote(companyName, safe='')
   
   if consumer == None: #Es deeplink sin codigo de pago
-    deeplink = f"https://yape.com.pe/app/services-pay/pickService?logo={ruta_logo}&companyId={company_id}&name={companyName}&serviceId={service_id}"
+    deeplink = f"https://yape.com.pe/app/services-pay/pickService?logo={ruta_logo}&companyId={company_id}&name={company_encoded}&serviceId={service_id}"
   else:
-    deeplink = f"https://yape.com.pe/app/services-pay/pickService?logo={ruta_logo}&companyId={company_id}&name={companyName}&serviceId={service_id}&consumerCode={normalize(consumer)}"
+    deeplink = f"https://yape.com.pe/app/services-pay/pickService?logo={ruta_logo}&companyId={company_id}&name={company_encoded}&serviceId={service_id}&consumerCode={normalize(consumer)}"
   
   logger.info(f"Deeplink generado: {deeplink}")
 
